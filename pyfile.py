c01154eb59c97626d1f3d867b15c6e07bad75e13
Maryam.KHS <maryamkhoshnevis904@gmail.com>
2024-04-22 13:14:25 +0430

    pyfile added for 2nd middle project

---
+++ pyfile.py
@@ -0,0 +1,42 @@
+lm=[]
+count=0
+result=0
+max=0
+
+def list_maghsoom():
+    n=num//2
+    for i in range(1,n+1):
+        if num%i==0:
+            lm.append(num//i)
+    return(lm)
+
+def tedad_aval():
+    c=0
+    #i=1
+    #count=0
+    n2=item
+    for i in range(1,n2+1):
+        if n2%i==0:
+            c+=1
+    #print(c)
+    return(c)
+    
+        
+
+
+for i in range (1,11):
+    num=int(input())
+    list_maghsoom()
+    for item in lm:
+        c=tedad_aval()
+        if c==2:
+            count+=1
+    if count>max:
+        max=count
+        result=num
+    elif count==max and num>result:
+        result=num
+    lm=[]
+    count=0
+#print(lm)
+print(result,' ',max)
